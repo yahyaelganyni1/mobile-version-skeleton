a4a94751139847e2fd098c4aa10ea6420287e306
yahyaelganyni1 <crazy20046@gmail.com>
2021-06-21 17:40:18 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-const mobileMenuCloseIcon = document.querySelector(".mobile-menu-close-icon");
-const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
-const menuLink = document.querySelectorAll(".menu-link");
+const mobileMenuCloseIcon = document.querySelector('.mobile-menu-close-icon');
+const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+const menuLink = document.querySelectorAll('.menu-link');
 
 mobileMenuCloseIcon.addEventListener('click', (e) => {
-  document.querySelector(".header-desctop").style.display = "none";
+  document.querySelector('.header-desctop').style.display = 'none';
 });
 
 mobileMenuIcon.addEventListener('click', (e) => {
-  document.querySelector(".header-desctop").style.display = "unset";
+  document.querySelector('.header-desctop').style.display = 'unset';
 });
 
 for (let i = 0; i < menuLink.length; i++) {
   menuLink[i].addEventListener('click', (e) => {
-    document.querySelector(".header-desctop").style.display = "none";
+    document.querySelector('.header-desctop').style.display = 'none';
   });
 }
