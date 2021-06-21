8f1be5983a06d3c1a6d9bd59464236f01e9ebbcc
Akasha <Akasha.Rojee@gmail.com>
2021-06-21 19:27:37 +0400

    Add js for menu link

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const mobileMenuCloseIcon = document.querySelector(".mobile-menu-close-icon");
 const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
+const menuLink = document.querySelectorAll(".menu-link");
 
 mobileMenuCloseIcon.addEventListener('click', (e) => {
   document.querySelector(".header-desctop").style.display = "none";
@@ -8,3 +9,9 @@ mobileMenuCloseIcon.addEventListener('click', (e) => {
 mobileMenuIcon.addEventListener('click', (e) => {
   document.querySelector(".header-desctop").style.display = "unset";
 });
+
+for (let i = 0; i < menuLink.length; i++) {
+  menuLink[i].addEventListener('click', (e) => {
+    document.querySelector(".header-desctop").style.display = "none";
+  });
+}
