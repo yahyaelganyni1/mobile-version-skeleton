c88a3ed2fd9a0c85c04bc47ee1402fe6e17d9963
yahyaelganyni1 <crazy20046@gmail.com>
2021-06-21 23:06:58 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 const mobileIcon = document.querySelector('.mobile-menu-icon');
 const mobileIconClose = document.querySelector('.mobile-menu-close-icon');
+const liLinks = document.querySelectorAll('.li-link');
 
 mobileIcon.addEventListener('click', () => {
   document.getElementById('mobile-menu').style.display = 'unset';
 });
 mobileIconClose.addEventListener('click', () => {
   document.getElementById('mobile-menu').style.display = 'none';
+});
+
+liLinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    document.getElementById('mobile-menu').style.display = 'none';
+  });
 });
