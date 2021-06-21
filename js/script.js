5203e3aa0c50f41ed77a4554829850140134a801
yahyaelganyni1 <crazy20046@gmail.com>
2021-06-21 17:47:06 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ const mobileMenuCloseIcon = document.querySelector('.mobile-menu-close-icon');
 const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
 const menuLink = document.querySelectorAll('.menu-link');
 
-mobileMenuCloseIcon.addEventListener('click', (e) => {
+mobileMenuCloseIcon.addEventListener('click', () => {
   document.querySelector('.header-desctop').style.display = 'none';
 });
 
-mobileMenuIcon.addEventListener('click', (e) => {
+mobileMenuIcon.addEventListener('click', () => {
   document.querySelector('.header-desctop').style.display = 'unset';
 });
 
 for (let i = 0; i < menuLink.length; i++) {
-  menuLink[i].addEventListener('click', (e) => {
+  menuLink[i].addEventListener('click', () => {
     document.querySelector('.header-desctop').style.display = 'none';
   });
 }
